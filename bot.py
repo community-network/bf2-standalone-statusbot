09fdc496fee2f8ec711f01e0cbf06aa948849ff1
Zefanja Jobse <zefanjaj@gmail.com>
2022-07-01 15:37:50 +0200

    also change name inside the discord group

--- bot.py
+++ bot.py
@@ -1,20 +1,15 @@
 import asyncio
 import sys
+from os import path, environ
 from opengsq.protocols import GameSpy3
 import discord
-import os
-import aiohttp
 
 """BF4 ONLY"""
 # CONFIG
-BOT_TOKEN = os.environ['token']
-SERVER_IP = os.environ['ip']
-SERVER_PORT = os.environ['port']
-BF2_MAPS = ['Dalian Plant', 'Daqing Oilfields', 'Dragon Valley', 'Fushe Pass', 'Greatwall', 'Gulf Of Oman',
-            'Highway Tampa', 'Kubra Dam', 'Mashtuur City', 'Midnight Sun', 'Operation Blue Pearl',
-            'Operation Clean Sweep', 'Operation Harvest', 'Operation Road Rage', 'Operation Smoke Screen',
-            'Road To Jalalabad', 'Sharqi Peninsula', 'Songhua Stalemate', 'Strike At Karkand', 'Taraba Quarry',
-            'Wake Island 2007', 'Zatar Wetlands']
+BOT_TOKEN = environ['token']
+SERVER_IP = environ['ip']
+SERVER_PORT = environ['port']
+GUILD_ID = environ['guild']
 
 class LivePlayercountBot(discord.Client):
     """Discord bot to display the BL true playercount in the bot status"""
@@ -22,42 +17,50 @@ class LivePlayercountBot(discord.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}\n" f"Started monitoring server {SERVER_IP}:{SERVER_PORT}")
         status = ""
+        current_servername = ""
         current_mapname = ""
-        async with aiohttp.ClientSession() as session:
-            while True:
-                try:
-                    server_info, map_name = await get_playercount(session)
-                    if server_info == None:
-                        await self.change_presence(activity=discord.Game("¯\\_(ツ)_/¯ server not found"))
-                    else:
-                        if server_info != status:  # avoid spam to the discord API
-                            await self.change_presence(activity=discord.Game(server_info))
-                            status = server_info
-                        if current_mapname != map_name:
-                            if map_name in BF2_MAPS:
-                                map_slug = map_name.lower().replace(' ', '_')+'.jpg'
-                            else:
-                                map_slug = "default.jpg"
-                            
-                            with open(f"maps/{map_slug}", 'rb') as f:
-                                await self.user.edit(avatar=f.read())
-                            current_mapname = map_name
-                except Exception as e:
-                    print(e)
-                await asyncio.sleep(10)
+        while True:
+            try:
+                server_info, map_name, server_name = await get_playercount()
+                if server_info == None:
+                    await self.change_presence(activity=discord.Game("¯\\_(ツ)_/¯ server not found"))
+                else:
+                    if server_info != status:  # avoid spam to the discord API
+                        await self.change_presence(activity=discord.Game(server_info))
+                        status = server_info
+                        
+                    if current_servername != server_name: # avoid spam to the discord API
+                        guild = self.get_guild(int(GUILD_ID))
+                        await guild.me.edit(nick=server_name[:32])
+                        current_servername = server_name
+                        
+                    if current_mapname != map_name: # avoid spam to the discord API
+                        to_check = f"maps/{map_name.lower().replace(' ', '_')}.jpg"
+                        if path.exists(to_check):
+                            map_slug = to_check
+                        else:
+                            map_slug = "maps/default.jpg"
+                        
+                        with open(map_slug, 'rb') as f:
+                            await self.user.edit(avatar=f.read())
+                        current_mapname = map_name
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(10)
  
-async def get_playercount(session):
+async def get_playercount():
     try:
         source = GameSpy3(address=SERVER_IP, query_port=int(SERVER_PORT))
         data = await source.get_status()
         max_slots = int(data["info"]["maxplayers"])
         map_name = data['info']["mapname"]
+        server_name = data['info']["hostname"]
         true_playercount = int(data["info"]["numplayers"])
         server_info = f"{true_playercount}/{max_slots} - {map_name}"  # discord status message
-        return server_info, map_name
+        return server_info, map_name, server_name
     except Exception as e:
         print(f"Error getting data from server: {e}")  # BL autism
-        return None, None
+        return None, None, None
  
 if __name__ == "__main__":
     assert sys.version_info >= (3, 7), "Script requires Python 3.7+"
